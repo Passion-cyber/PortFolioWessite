cc9d5efb0f47637531803e9551d86068c76e4a25
Ibelachi Chinedu Franklyn <chineduibelachi@gmail.com>
2022-11-11 16:11:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+const openButton = document.querySelector(".open-btn");
+const closeButton = document.querySelector(".close-btn");
+const navContainer = document.querySelector("nav");
+
+openButton.addEventListener("click", () => {
+  navContainer.classList.add("open-nav");
+});
+
+closeButton.addEventListener("click", () => {
+  navContainer.classList.remove("open-nav");
+});
+
+function outsideClick(e) {
+  if ("click" == navContainer.classList.remove("open-nav"));
+  navContainer.classList.remove("open-nav");
+}
+
+// Tech Scroll
+window.sr = ScrollReveal();
+sr.reveal(".logo", {
+  duration: 2000,
+  origin: "top",
+  distance: "100px",
+});
+
+sr.reveal("nav li", {
+  duration: 2000,
+  origin: "top",
+  distance: "100px",
+});
+
+sr.reveal(".hero-text-wrapper", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+});
+
+sr.reveal(".hero-image-wrapper", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".services-text", {
+  duration: 2000,
+  origin: "left",
+  viewFactor: 0.2,
+  distance: "200px",
+});
+
+sr.reveal(".services", {
+  duration: 2000,
+  origin: "bottom",
+  viewFactor: 0.2,
+  distance: "200px",
+});
+
+sr.reveal(".section-1", {
+  duration: 2000,
+  origin: "bottom",
+  viewFactor: 0.2,
+  distance: "200px",
+});
+
+sr.reveal(".wrap-left", {
+  duration: 2000,
+  origin: "left",
+  viewFactor: 0.2,
+  distance: "200px",
+});
+
+sr.reveal(".wrap-right", {
+  duration: 2000,
+  origin: "right",
+  viewFactor: 0.2,
+  distance: "200px",
+});
+
+sr.reveal(".individual-items", {
+  duration: 2000,
+  origin: "bottom",
+  viewFactor: 0.2,
+  distance: "300px",
+});
+
+sr.reveal(".contact-text", {
+  duration: 2000,
+  origin: "left",
+  viewFactor: 0.2,
+  distance: "300px",
+});
+
+sr.reveal(".contact-button", {
+  duration: 2000,
+  origin: "right",
+  viewFactor: 0.2,
+  distance: "300px",
+});
+sr.reveal(".footer-logo", {
+  duration: 2000,
+  origin: "left",
+  viewFactor: 0.2,
+  distance: "300px",
+});
+sr.reveal(".footer-nav", {
+  duration: 2000,
+  origin: "right",
+  viewFactor: 0.2,
+  distance: "200px",
+});
+sr.reveal(".footer-icons", {
+  duration: 2000,
+  origin: "left",
+  viewFactor: 0.2,
+  distance: "300px",
+});
+sr.reveal(".title", {
+  duration: 2000,
+  origin: "right",
+  viewFactor: 0.2,
+  distance: "200px",
+});
+
+sr.reveal(".hero-resume", {
+  duration: 2000,
+  origin: "bottom",
+  delay: 1000,
+  distance: "50px",
+  easing: "ease-in-out",
+});
+
+
+
+// Smooth Scrolling Effect On Navbar
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
